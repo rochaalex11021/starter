453423b252e87f7c598321c665145b5fe5fd757f
rochaalex11021 <rochaalex11021>
2022-09-01 20:58:35 -0500

    practice

---
+++ assigntments.js
@@ -0,0 +1,41 @@
+/*let country = 'Mexico';
+let continent = 'North America';
+let population = '129';
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+ let language;
+ console.log(typeof isIsland);
+ console.log(typeof population);
+ console.log(typeof country);
+ console.log(typeof language);
+
+
+ let isIsland = false;
+ let language;
+ console.log(typeof isIsland);
+ console.log(typeof population);
+ console.log(typeof country);
+ console.log(typeof language);
+
+ language = 'spanish';
+ const country = 'Mexico';
+ const continent = 'North America';
+ const isIsland = false;
+ isIsland = true;*/
+
+let population = '129';
+let country = 'Mexico';
+let continent = 'North America';
+let language = 'Spanish';
+
+ console.log(population / 2);
+ population++;
+ console.log(population);
+ console.log(population > 6);
+ console.log(population < 33);
+ const description1 =
+ `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+ console.log(description1);
--- script.js
+++ script.js
@@ -237,11 +237,4 @@ const massMark = 78;
  }
 */
 
-let isIsland = false;
-let language;
-
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
 
